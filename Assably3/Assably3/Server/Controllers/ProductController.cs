58d5e56f7944b682cf1516cc81f1d99132e3af70
Billie <elliot.agustin.martinez.macias@uabc.edu.mx>
2022-09-27 19:54:02 -0700

    Creación de Controller

---
+++ Assably3/Assably3/Server/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assably3.Server.Service;
+
+namespace Assably3.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller")]
+    public class ProductController : Controller
+    {
+        private ProductServices service;
+
+    }
+}
---
+++ Assably3/Assably3/Shared/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assably3.Shared
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public Double Price { get; set; }
+        public String Description { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
