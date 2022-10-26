f7e9f67654bc77e1c143ccfb5772c1602f6d16b1
Billieeeeee <91598348+Billieeeeee@users.noreply.github.com>
2022-10-26 01:01:07 -0700

    Add files via upload

---
+++ BdEjemplo/BdEjemplo/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace DataBaseExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "Sumi\\SQLEXPRESS";
+                builder.InitialCatalog = "Bd_P5";
+                builder.IntegratedSecurity = true;
+
+                builder.TrustServerCertificate = true;
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+
+                    String sql = "SELECT FirstName, LastName FROM Customer";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("\nDone. Press enter.");
+            Console.ReadLine();
+        }
+    }
+}
